652c25d73df61c0271086c28abeaaa0606c110eb
Julio Muller <juliodolago@gmail.com>
2020-12-09 02:06:56 -0300

    Changed displayable screen title

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,8 +12,22 @@ export const TO_DETAILS = 'MovieDetails'
 const Routes = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name={TO_HOME} component={Home} />
-      <Stack.Screen name={TO_DETAILS} component={MovieDetails} />
+      <Stack.Screen
+        name={TO_HOME}
+        component={Home}
+        options={{
+          title: 'THE MOVIE DATABASE',
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name={TO_DETAILS}
+        component={MovieDetails}
+        options={{
+          title: 'Detalhes',
+          headerTitleAlign: 'center',
+        }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 )
