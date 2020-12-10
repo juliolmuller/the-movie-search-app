844ea3c4f84f8586a3d4d508246e0276d2166836
Julio Muller <juliodolago@gmail.com>
2020-12-10 00:29:12 -0300

    Updated application constants

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,11 +1,23 @@
 import axios from 'axios'
 import Constants from 'expo-constants'
 
+const {
+  TMDB_API_KEY,
+  TMDB_API_URL,
+  TMDB_PICTURE_URL,
+  TMDB_THUMBNAIL_URL,
+} = Constants.manifest.extra
+
 const http = axios.create({
-  baseURL: Constants.manifest.extra.API_URL,
+  baseURL: TMDB_API_URL,
   params: {
-    api_key: Constants.manifest.extra.API_KEY,
+    api_key: TMDB_API_KEY,
   },
+})
+
+Object.defineProperties(http, {
+  imageURL: { get: () => TMDB_PICTURE_URL },
+  thumbURL: { get: () => TMDB_THUMBNAIL_URL },
 })
 
 export default http
