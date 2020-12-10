a4c8d0ee76f6fdd2e1bea9fea286c0333c8edbb8
Julio Muller <juliodolago@gmail.com>
2020-12-10 00:31:05 -0300

    Created  MovieCard to display search results

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'import',
   ],
   rules: {
+    'camelcase': ['error', { allow: ['backdrop_path', 'include_adult', 'release_date', 'vote_average'] }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-use-before-define': 'off',
     'react/jsx-indent-props': ['error', 2],
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,77 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import tmdb from '../services/tmdb'
 
-const MovieCard = ({ title }) => (
+const MovieCard = ({ backdrop_path, release_date, title, vote_average }) => (
   <View style={styles.card}>
-    <Text style={styles.title}>
-      {title}
-    </Text>
+    <Image
+      style={styles.thumbnail}
+      source={{ uri: tmdb.thumbURL + backdrop_path }}
+    />
+    <View style={styles.details}>
+      <Text style={styles.year}>
+        {new Date(release_date).getFullYear()}
+      </Text>
+      <Text style={styles.title}>
+        {title}
+      </Text>
+    </View>
+    <View style={styles.evaluation}>
+      <Text style={[styles.rate, styles.scale(vote_average)]}>
+        {vote_average.toFixed(1)}
+      </Text>
+    </View>
   </View>
 )
 
 const styles = StyleSheet.create({
   card: {
+    width: '100%',
     flexDirection: 'row',
-    backgroundColor: '#111',
-    marginBottom: 10,
-    padding: 4,
+    backgroundColor: '#eee',
+    marginVertical: 5,
+  },
+  thumbnail: {
+    resizeMode: 'cover',
+    width: 100,
+    height: 75,
+  },
+  details: {
+    flex: 1,
+    paddingHorizontal: 5,
   },
   title: {
+    color: '#111',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  year: {
+    color: '#333',
+  },
+  evaluation: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  rate: {
+    borderRadius: 10,
+    padding: 6,
     color: '#fff',
-    fontSize: 20,
+  },
+  scale(rate) {
+    let backgroundColor
+
+    if (rate >= 7.5) {
+      backgroundColor = '#21c239'
+    } else if (rate >= 5) {
+      backgroundColor = '#f5cb42'
+    } else if (rate >= 2.5) {
+      backgroundColor = '#c25f21'
+    } else {
+      backgroundColor = '#f00'
+    }
+
+    return { backgroundColor }
   },
 })
 
