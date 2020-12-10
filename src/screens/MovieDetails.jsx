7e249765bd3f5f1b93edf13511a3f5ec297fef5b
Julio Muller <juliodolago@gmail.com>
2020-12-10 08:43:01 -0300

    Added backdrops to MovieDetails

--- src/screens/MovieDetails.jsx
+++ src/screens/MovieDetails.jsx
@@ -1,19 +1,43 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import tmdb from '../services/tmdb'
 
 const MovieDetails = ({ route }) => {
-  const [movie, setMovie] = useState(null)
+  const [movie, setMovie] = useState(route.params)
 
   useEffect(() => {
-    tmdb
-      .get(`/movie/${route.params.id}`)
-      .then(({ data }) => setMovie(data))
-      .catch(console.log)
+    const responses = Promise.all([
+      tmdb.get(`/movie/${route.params.id}`),
+      tmdb.get(`/movie/${route.params.id}/images`),
+    ])
+
+    responses
+      .then(([movieResponse, backdropResponse]) => {
+        const movieData = movieResponse.data
+        const { backdrops } = backdropResponse.data
+        setMovie({ ...movieData, backdrops })
+      })
+      .catch((error) => {
+        console.log(error)
+        alert('ERROR: Unable to connect to server.')
+      })
   }, [route.params.id])
 
-  return movie && (
+  return (
     <View style={styles.wrapper}>
+      <View style={styles.backdropContainer}>
+        <ScrollView horizontal pagingEnabled>
+          {movie.backdrops ? (
+            movie.backdrops.map(({ file_path: uri }) => (
+              <Image key={uri} style={styles.backdrop} source={{ uri: tmdb.thumbURL + uri }} />
+            ))
+          ) : (
+            <Image style={styles.backdrop} source={{ uri: tmdb.thumbURL + movie.backdrop_path }} />
+          )}
+        </ScrollView>
+      </View>
+
       <Text style={styles.title}>
         {movie.title}
       </Text>
@@ -24,6 +48,14 @@ const MovieDetails = ({ route }) => {
 const styles = StyleSheet.create({
   wrapper: {
     flexGrow: 1,
+  },
+  backdropContainer: {
+    height: 240,
+  },
+  backdrop: {
+    width: Dimensions.get('window').width,
+    height: 240,
+    resizeMode: 'cover',
   },
   title: {
     textAlign: 'center',
