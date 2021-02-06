9d7d7863287975940d88ba86308725ac7b74cc73
Julio Muller <juliodolago@gmail.com>
2021-02-05 22:33:07 -0300

    Updated ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,13 +31,17 @@ module.exports = {
     'jsx-control-statements',
   ],
   rules: {
-    'camelcase': ['error', { allow: ['backdrop_path', 'episode_count', 'file_path', 'first_air_date', 'include_adult', 'known_for_department', 'place_of_birth', 'profile_path', 'release_date', 'vote_average'] }],
+    'camelcase': ['error', { allow: ['api_key', 'backdrop_path', 'episode_count', 'file_path', 'first_air_date', 'include_adult', 'known_for_department', 'place_of_birth', 'profile_path', 'release_date', 'vote_average'] }],
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     'no-confusing-arrow': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-use-before-define': 'off',
     'object-curly-newline': ['error', { ImportDeclaration: { multiline: true, minProperties: 8 } }],
     'react/jsx-indent-props': ['error', 2],
     'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-no-undef': ['error', { allowGlobals: true }],
     'react/prop-types': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-raw-text': 'off',
   },
 }
